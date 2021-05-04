153103f160be7c40e10c7780d121c5650ead2916
BecaLParker <becaparker@gmail.com>
2021-05-04 15:50:57 +0100

    failing test for a freshly initialised card has a balance of 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oyster-card.rb'
 
 describe Oystercard do
-    
+ describe '#balance'do
+    it 'a freshly initialised card has a balance of 0' do
+        card = Oystercard.new
+        expect(card.balance).to eq 0
+    end
+end
 end
